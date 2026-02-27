3451a6ade16c79a1701ad15f795ecf51a9d8f8a3
62761 <z.mehdi.dev@gmail.com>
2026-02-27 21:25:32 +0100

    feat(iac): add apprunner ECR access role

---
+++ infra/terraform/iam_apprunner_ecr.tf
@@ -0,0 +1,23 @@
+data "aws_iam_policy_document" "apprunner_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["build.apprunner.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "apprunner_ecr_access" {
+  name               = "${var.project_name}-apprunner-ecr-access"
+  assume_role_policy = data.aws_iam_policy_document.apprunner_assume.json
+}
+
+resource "aws_iam_role_policy_attachment" "apprunner_ecr_access" {
+  role       = aws_iam_role.apprunner_ecr_access.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
+}
+
+output "apprunner_ecr_access_role_arn" {
+  value = aws_iam_role.apprunner_ecr_access.arn
+}
